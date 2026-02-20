39b690b980a9a0d849ee239614503d6a93cae847
Abhay Kumar <abhay13042003@gmail.com>
2026-02-20 13:41:12 +0530

    fix gem api

--- src/Component/Chatbot.jsx
+++ src/Component/Chatbot.jsx
@@ -6,7 +6,7 @@ import { FaPaperPlane, FaComments, FaTimes, FaRobot } from "react-icons/fa";
 
 const Chatbot = () => {
   const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=";
-  const API_KEY = "AIzaSyCmLGVgwrvAfeuDjQV_nCWrYvyGDx5RAlg";
+  const API_KEY = "AIzaSyBgb-IOlPhLP36wat6ViTRkp-aRq9Ku6bU";
 
   const initialPrompt = `You are a helpful and friendly chatbot for a doctor's office appointment system. 
 Your primary goal is to book appointments for patients. Your task is to ask the user for the following information in a clear and conversational manner, one question at a time:
